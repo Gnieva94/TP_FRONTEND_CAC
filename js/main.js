6dd8696456a5c32ca3fad4512d9da1efe267fc43
Gnieva94 <gus_94@hotmail.com.ar>
2023-05-23 03:08:55 -0300

    validaciones

--- js/main.js
+++ js/main.js
@@ -8,21 +8,33 @@ const listaCategorias={
     'Junior':15,
 }
 const btnResumen = document.getElementById('Resumen')
-btnResumen.addEventListener('click',()=>{
+btnResumen.addEventListener('click',(e)=>{
+    e.preventDefault()
+    const nombre = document.getElementById('Nombre').value
+    const apellido = document.getElementById('Apellido').value
+    const correo = document.getElementById('Correo').value
+    if(nombre=="" || apellido=="" || correo==""){
+        alert("Ingrese todos los datos")
+        return
+    }
     const lblPagar = document.getElementById('form-pagar')
     const cantidad = document.getElementById('Cantidad').value
-    if(cantidad>0){
-        const categoria = document.getElementById('Categoria')
-        const categoriaSeleccionado = categoria.options[categoria.selectedIndex].text;
-        let descuento
-        for (const key in listaCategorias) {
-            if(key==categoriaSeleccionado){
-                descuento=listaCategorias[key]
-            }
-        }
-        console.log(descuento)
-        lblPagar.textContent=`Total a Pagar: $ ${total(cantidad,descuento)}`
+    if(cantidad==""){
+        alert("Ingrese cantidad")
     }else{
-        alert("Cantidad incorrecta");
+        if(cantidad>0){
+            const categoria = document.getElementById('Categoria')
+            const categoriaSeleccionado = categoria.options[categoria.selectedIndex].text;
+            let descuento
+            for (const key in listaCategorias) {
+                if(key==categoriaSeleccionado){
+                    descuento=listaCategorias[key]
+                }
+            }
+            console.log(descuento)
+            lblPagar.textContent=`Total a Pagar: $ ${total(cantidad,descuento)}`
+        }else{
+            alert("Cantidad incorrecta");
+        }
     }
 })
